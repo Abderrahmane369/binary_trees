0e093584c41398c166f63fe6a842535e49bcd1b9
hachimB <boubacarhachim@gmail.com>
2023-11-28 18:05:25 +0100

    even

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,12 +7,12 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-if (!parent)
+if (parent == NULL)
 return (NULL);
 binary_tree_t *new_node = binary_tree_node(parent, value);
-if (!new_node)
+if (new_node == NULL)
 return (NULL);
-if (parent->right)
+if (parent->right != NULL)
 {
 new_node->right = parent->right;
 new_node->right->parent = new_node;
