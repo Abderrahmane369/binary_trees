78fa1f1141f8dfcb15210e3de3040112b8dc0cad
Abderrahmane369 <cszamasu@gmail.com>
2023-11-28 20:58:09 +0100

    ODD

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+binary_tree_insert_left - insert node left
+@parent: parent
+@value: value
+Return: binary_tree_t
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+				binary_tree_t *newNode;
+
+				if (!parent)
+								return (NULL);
+
+				newNode = malloc(sizeof(binary_tree_t));
+				
+				if (!newNode)
+								return (NULL);
+
+				newNode->left = parent->left;
+				newNode->right = NULL;
+				newNode->n = value;
+				newNode->parent = parent;
+				
+				if (parent->left)
+								parent->left->parent = newNode;
+
+				parent->left = newNode;
+				
+				return (newNode);
+}
+
---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+*/
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+				if (!func || !tree)
+								return;
+
+				binary_tree_inorder(tree->left, func);
+				func(tree->n);
+				binary_tree_inorder(tree->right, func);
+}
+
