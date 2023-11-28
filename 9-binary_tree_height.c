074649a614f443cb1d90b2209c174a8b928b474f
Abderrahmane369 <cszamasu@gmail.com>
2023-11-28 23:02:24 +0100

    UPDATE

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_delete - deletor
- *@tree: tree
+ * binary_tree_height - deletor
+ * @tree: tree
+ * Return: height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-				size_t leftH, rightH;
+	size_t leftH, rightH;
 
-				if (!tree)
-								return (-1);
+	if (!tree)
+		return (0);
 
-				leftH = binary_tree_height(tree->left) + 1;
-				rightH = binary_tree_height(tree->right) + 1;
+	leftH = binary_tree_height(tree->left);
+	rightH = binary_tree_height(tree->right);
 
-				return (leftH > rightH ? leftH : rightH);
+	return ((leftH > rightH ? leftH : rightH) + 1);
 }
-
