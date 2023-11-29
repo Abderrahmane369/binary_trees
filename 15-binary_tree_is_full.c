73300ec6c75fbe588f88660b1d8540740d3d644f
Abderrahmane369 <cszamasu@gmail.com>
2023-11-30 00:35:36 +0100

    UPDATE

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - is it full?
+ * @tree: the tree
+ * Return: 1 if full or 0 otherwise
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+				int l, r;
+
+				if (!tree)
+								return (0);
+
+				if ((tree->left != NULL) + (tree->right != NULL) == 1)
+								return (0);
+
+				l = binary_tree_is_full(tree->left);
+				r = binary_tree_is_full(tree->right);
+				return ((l + r + 1) % 2);
+}
--- main.c
+++ main.c
@@ -10,26 +10,21 @@
 int main(void)
 {
     binary_tree_t *root;
-    binary_tree_t *sibling;
+    int full;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 128);
-    root->left->right = binary_tree_node(root->left, 54);
-    root->right->right = binary_tree_node(root->right, 402);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root, 128);
     root->left->left = binary_tree_node(root->left, 10);
-    root->right->left = binary_tree_node(root->right, 110);
-    root->right->right->left = binary_tree_node(root->right->right, 200);
-    root->right->right->right = binary_tree_node(root->right->right, 512);
-
     binary_tree_print(root);
-    sibling = binary_tree_sibling(root->left);
-    printf("Sibling of %d: %d\n", root->left->n, sibling->n);
-    sibling = binary_tree_sibling(root->right->left);
-    printf("Sibling of %d: %d\n", root->right->left->n, sibling->n);
-    sibling = binary_tree_sibling(root->left->right);
-    printf("Sibling of %d: %d\n", root->left->right->n, sibling->n);
-    sibling = binary_tree_sibling(root);
-    printf("Sibling of %d: %p\n", root->n, (void *)sibling);
+
+    full = binary_tree_is_full(root);
+    printf("Is %d full: %d\n", root->n, full);
+    full = binary_tree_is_full(root->left);
+    printf("Is %d full: %d\n", root->left->n, full);
+    full = binary_tree_is_full(root->right);
+    printf("Is %d full: %d\n", root->right->n, full);
     return (0);
 }
