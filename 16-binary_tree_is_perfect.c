2bf281994bd34ad27cb9a36a8ac592a486a59617
hachimB <boubacarhachim@gmail.com>
2023-11-29 11:34:55 +0100

    even

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,7 +25,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 size_t left_height, right_height;
 if (!tree)
-return (1);
+return (0);
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 return ((left_height == right_height) &&
